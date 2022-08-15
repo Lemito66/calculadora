0b20873c947c9a5bf2b5bd25150eea14e66a1f0b
Emill <lemito66@gmail.com>
2022-08-15 16:19:04 -0500

    Comenzando

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+          src={}
+          className='freecodecamp-logo'
+          alt='Logo de freeCodeCamp' />
+      </div>
+    </div>
+  );
+}
+
+export default App;
